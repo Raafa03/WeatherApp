cea83b9325d26fbcf2b3560f7e2b46af983de488
Rafael Estevam <rafael2003.estevam@gmail.com>
2023-10-26 20:30:14 +0100

    26/10/2023

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -3,13 +3,35 @@ package com.example.weatherapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.node.modifierElementOf
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,59 +41,94 @@ class MainActivity : ComponentActivity() {
             WeatherAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
                 }
+
+                HomeScreen()
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WeatherAppTheme {
-        Greeting("Android")
-    }
-}
 
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, heightDp = 800, widthDp = 390)
 @Composable
 fun HomeScreen(): Unit {
-    
+    Column() {
+        NavegacaoeEPesquisaComponent()
+        WelcomeMessageComponent()
+        ImagePreviewComponent()
+        PrevisaoComponent()
+    }
+
+
 }
 
 
-@Preview(showBackground = true)
 @Composable
 fun NavegacaoeEPesquisaComponent(): Unit {
-    
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically)
+
+
+    {
+        Button(onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent, contentColor = Color.White)) {
+            Image(
+                painter = painterResource(id = R.drawable.lupa),
+                contentDescription = null,
+                modifier = Modifier.size(50.dp)
+
+            )
+            
+
+        }
+    }
+
+
+
 }
 
 
-@Preview(showBackground = true)
+
 @Composable
 fun WelcomeMessageComponent(): Unit {
+
+    val borderWidth = 4.dp
+    Image(
+        painter = painterResource(id = R.drawable.welcome),
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .size(150.dp)
+            .border(
+                BorderStroke(borderWidth, Color.Yellow),
+                RectangleShape
+            )
+            .padding(borderWidth)
+            .clip(RectangleShape)
+
+
+    )
+
+
     
 }
 
 
-@Preview(showBackground = true)
+
 @Composable
 fun ImagePreviewComponent(): Unit {
     
 }
 
 
-@Preview(showBackground = true)
+
 @Composable
 fun PrevisaoComponent(): Unit {
     
